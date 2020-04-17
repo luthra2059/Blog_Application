db5ca8c86b2478e3920bc0713e369b0857fc0b65
Shubham Luthra <luthra2059@gmail.com>
2020-04-17 07:24:22 +0530

    Application Post Ready

--- Blog_Application/urls.py
+++ Blog_Application/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', home_page),
-    path('about/', about),
-    path('contact/', contact),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]

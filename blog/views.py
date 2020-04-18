4ed537a55e576f1d3a330a784d5e3d4fa1b5f865
Shubham Luthra <luthra2059@gmail.com>
2020-04-18 06:29:00 +0530

    pagination added

--- blog/views.py
+++ blog/views.py
@@ -9,24 +9,8 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 3
+    paginate_by = 1
 
-# def PostList(request):
-#     object_list = Post.objects.filter(status=1).order_by('-created_on')
-#     paginator = Paginator(object_list, 3)  # 3 posts in each page
-#     page = request.GET.get('page')
-#     try:
-#         post_list = paginator.page(page)
-#     except PageNotAnInteger:
-#             # If page is not an integer deliver the first page
-#         post_list = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range deliver last page of results
-#         post_list = paginator.page(paginator.num_pages)
-#     return render(request,
-#                   'index.html',
-#                   {'page': page,
-#                    'post_list': post_list})
 
 def post_detail(request, slug):
     template_name = 'post_detail.html'

b165bcc6f9f5bbacdb5cbfd28eb487e1987d30a5
Shubham Luthra <luthra2059@gmail.com>
2020-04-18 07:32:01 +0530

    added atom feeds

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
-
+from .feeds import LatestPostsFeed
 sitemaps = {
     "posts": PostSitemap,
 }
@@ -21,6 +21,7 @@ urlpatterns = [
      path('<slug:slug>/', views.post_detail, name='post_detail'),
    path('summernote/',include('django_summernote.urls')),
 path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+path("feed/rss", LatestPostsFeed(), name="post_feed"),
     
 ]
 
